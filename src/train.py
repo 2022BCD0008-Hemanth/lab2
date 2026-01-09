f428afebe9397768d7800f3c8c38eb9753b17883
aHemanth123 <hemanthsaimanikantaappari@gmail.com>
2026-01-09 21:16:30 +0530

    EXP-07: Linear Regression with standardization

--- src/train.py
+++ src/train.py
@@ -1,18 +1,24 @@
 import json, joblib, pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 ROLL_NO = "2022BCD0008_hemanth"
-MODEL_NAME = "Random Forest (50 trees, max_depth=10)"
+MODEL_NAME = "Linear Regression + Standardization"
 
 df = pd.read_csv("data/winequality-red.csv")
+
 X = df.drop("quality", axis=1)
 y = df["quality"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestRegressor(n_estimators=50, max_depth=10, random_state=42)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
