3bd02f2bcc8f3da73fa1d68ef63b2916987633b4
aHemanth123 <hemanthsaimanikantaappari@gmail.com>
2026-01-09 20:27:24 +0530

    EXP-03: Decision Tree Regressor

--- src/train.py
+++ src/train.py
@@ -2,36 +2,40 @@ import json
 import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Lasso
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 ROLL_NO = "2022BCD0008_hemanth"
-MODEL_NAME = "Lasso Regression (alpha=0.01)"
-
+MODEL_NAME = "Decision Tree Regressor (max_depth=10)"
+ 
 df = pd.read_csv("data/winequality-red.csv")
 
 X = df.drop("quality", axis=1)
 y = df["quality"]
-
+ 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = Lasso(alpha=0.01)
+# Model
+model = DecisionTreeRegressor(max_depth=10, random_state=42)
 model.fit(X_train, y_train)
 
+# Prediction
 y_pred = model.predict(X_test)
 
+# Metrics
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
+# Logs (NO accuracy)
 print("Roll No:", ROLL_NO)
 print("Model:", MODEL_NAME)
 print("MSE:", mse)
-print("R²:", r2) 
-
+print("R²:", r2)
+ 
 joblib.dump(model, "outputs/model.pkl")
-
+ 
 results = {
     "roll_no": ROLL_NO,
     "model": MODEL_NAME,
